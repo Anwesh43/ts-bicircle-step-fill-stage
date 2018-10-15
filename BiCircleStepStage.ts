5056b0885db8a00a35d2e824a874d54d388a7a87
Anwesh43 <anweshthecool0@gmail.com>
2018-10-15 13:18:52 +0530

    adding BiCircleStep class

--- BiCircleStepStage.ts
+++ BiCircleStepStage.ts
@@ -144,3 +144,26 @@ class BCSNode {
         return this
     }
 }
+
+class BiCircleStep {
+    root : BCSNode = new BCSNode(0)
+    curr : BCSNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
