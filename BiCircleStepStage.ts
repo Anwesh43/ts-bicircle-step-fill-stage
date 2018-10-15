78557c8ac2c1c00bac786b5cea7c18c65abd8005
Anwesh43 <anweshthecool0@gmail.com>
2018-10-15 12:42:06 +0530

    adding draw methods

--- BiCircleStepStage.ts
+++ BiCircleStepStage.ts
@@ -1,5 +1,31 @@
 const w : number = window.innerWidth, h : number = window.innerHeight
+const color : string = '#673AB7'
 const nodes : number = 5
+const defineArc : Function = (context, r) => {
+  context.beginPath()
+  context.arc(0, 0, r, 0, 2 * Math.PI)
+}
+const drawBSNode : Function = (context, i, scale) => {
+    const gap : number = h / (nodes + 1)
+    const r : number = gap/3
+    context.fillStyle = color
+    context.strokeStyle = color
+    context.lineWidth = Math.min(w, h) / 60
+    context.save()
+    context.translate(this.i * gap + gap, h/2)
+    for(var j = 0; j < 2; j++) {
+        const sc : number = Math.min(0.5, Math.max(scale - 0.5 * j, 0)) * 2
+        context.save()
+        context.translate(-r + 2 * r * j, 0)
+        defineArc(context, r)
+        context.stroke()
+        defineArc(context, r)
+        context.clipPath()
+        context.fillRect(-r, -r, 2 * r * sc, 2 * r)
+        context.restore()
+    }
+    context.restore()
+}
 class BiCircleStepStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
